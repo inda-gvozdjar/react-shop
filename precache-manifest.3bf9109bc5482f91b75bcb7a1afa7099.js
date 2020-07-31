6e83245f08e3b95a3699209a438e5be301af5f24
Inda Gvozdjar <inda.gvozdjar@gmail.com>
2020-07-31 10:53:21 +0200

    Updates

---
+++ precache-manifest.3bf9109bc5482f91b75bcb7a1afa7099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cba81034f8ee4f86cd4820df5a97460",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "5f08e07afd34245be9d0",
+    "url": "/react-shop/static/css/main.1d804f33.chunk.css"
+  },
+  {
+    "revision": "ba21cfbf8411de5a7268",
+    "url": "/react-shop/static/js/2.6573cbc8.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/react-shop/static/js/2.6573cbc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f08e07afd34245be9d0",
+    "url": "/react-shop/static/js/main.c14b6888.chunk.js"
+  },
+  {
+    "revision": "6290e5b26abe9f80b39d",
+    "url": "/react-shop/static/js/runtime-main.33653570.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/react-shop/static/media/crown.19ef7b17.svg"
+  }
+]);
